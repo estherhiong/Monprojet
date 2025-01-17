6a89311a0d36406b90412968576d831c6c7a1968
Esther <eckisa@icloud.com>
2025-01-17 14:54:02 +0100

    first commit

---
+++ MonApplication/MonApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MonApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
